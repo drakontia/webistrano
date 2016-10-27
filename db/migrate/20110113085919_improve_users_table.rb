d653f5ee0e53f0e9800083a292894234d085bd33
drakontia <mhden@drakontia.com>
2016-10-27 16:23:26 +0900

    Fix database connection

--- db/migrate/20110113085919_improve_users_table.rb
+++ db/migrate/20110113085919_improve_users_table.rb
@@ -2,18 +2,18 @@ class ImproveUsersTable < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
       t.remove_index :disabled
+      t.change :admin, :boolean, default: false
       t.rename :disabled, :disabled_at
       t.index :disabled_at
     end
-    change_column :users, :admin, 'boolean USING CAST(admin AS boolean)'
   end
 
   def self.down
     change_table :users do |t|
       t.remove_index :disabled_at
+      t.change :admin, :integer, default: 0
       t.rename :disabled_at, :disabled
       t.index :disabled
     end
-    change_column :users, :admin, 'integer USING CAST(admin AS integer)'
   end
 end
