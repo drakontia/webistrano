6b1fdf71c2807f4d98d4cda824ccebb9b76ef0ed
drakontia <mhden@drakontia.com>
2016-10-27 15:58:38 +0900

    Fix postgresql error

--- db/migrate/20110113085919_improve_users_table.rb
+++ db/migrate/20110113085919_improve_users_table.rb
@@ -3,17 +3,17 @@ class ImproveUsersTable < ActiveRecord::Migration
     change_table :users do |t|
       t.remove_index :disabled
       t.rename :disabled, :disabled_at
-      t.change :admin, :default => false, 'boolean USING CAST(admin AS boolean)'
       t.index :disabled_at
     end
+    change_column :users, :admin, :boolean, default: false
   end
 
   def self.down
     change_table :users do |t|
       t.remove_index :disabled_at
       t.rename :disabled_at, :disabled
-      t.change :admin, :integer, :default => 0
       t.index :disabled
     end
+    change_column :users, :admin, :integer, default: 0
   end
 end
