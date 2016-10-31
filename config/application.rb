bd336e0d76adf1ffec73d377a50bc72a36cec994
drakontia <mhden@drakontia.com>
2016-10-31 17:16:25 +0900

    We need "require rake" for desc method?

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 require 'open4'
 require 'rails/all'
+require 'rake'
 require 'sshkit'
 require 'sshkit/dsl'
 include SSHKit::DSL
