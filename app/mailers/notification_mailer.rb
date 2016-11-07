4e34989a51b0fe9314c372b2e15106b944ff4250
drakontia <mhden@drakontia.com>
2016-11-07 11:55:02 +0900

    Change class name to be added mailer suffix

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,14 +1,14 @@
-class Notification < ActionMailer::Base
-  
+class NotificationMailer < ActionMailer::Base
+
   default :from => 'Webistrano'
-  
+
   def self.webistrano_sender_address=(val)
     default :from => val
   end
 
   def deployment(deployment, email)
     @deployment = deployment
-    
+
     mail(
       :to      => email,
       :subject => "Deployment of #{deployment.stage.project.name}/#{deployment.stage.name} finished: #{deployment.status}",
