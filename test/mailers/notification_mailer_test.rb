52eb626db7d3f76973537a5bcfdf836cc99d1048
drakontia <mhden@drakontia.com>
2016-11-07 11:56:39 +0900

    Change class name to be added mailer suffix

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -1,21 +1,21 @@
 require 'test_helper'
 
-class NotificationTest < ActionMailer::TestCase
-  tests Notification
-  
+class NotificationMailerTest < ActionMailer::TestCase
+  tests NotificationMailer
+
   FIXTURES_PATH = File.dirname(__FILE__) + '/../fixtures'
 
   test "sender_address" do
-    Notification.webistrano_sender_address = "FooBar"
-    
+    NotificationMailer.webistrano_sender_address = "FooBar"
+
     stage = FactoryGirl.create(:stage)
     role = FactoryGirl.create(:role, :stage => stage, :name => 'app')
     assert stage.deployment_possible?, stage.deployment_problems.inspect
     deployment = FactoryGirl.create(:deployment, :stage => stage, :task => 'deploy')
-    
-    email = Notification.deployment(deployment, 'foo@bar.com').deliver_now
+
+    email = NotificationMailer.deployment(deployment, 'foo@bar.com').deliver_now
     assert !ActionMailer::Base.deliveries.empty?
-    
+
     assert_equal ['foo@bar.com'], email.to
     assert_equal ["FooBar"], email.from
     assert_equal "Deployment of #{stage.project.name}/#{stage.name} finished: running", email.subject
